6b36f264551b369da1f5324b8a6011ebd0a4acd3
lin <23>
2022-02-23 22:26:14 +0800

    getchar

---
+++ mywork/This_is_test_for_getchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    while ((ch=getchar()) !='\n') {
+        /* code */
+        printf("copythat ");
+    }
+    while(1);
+    return 0;
+}
+//程序会停止，等待输入
+//输入abc 输出copy copy copy
